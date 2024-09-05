77296a382c93cda373325a60bfa23db022eb0365
Ali <aliahadmd1@gmail.com>
2024-09-05 23:51:20 +0600

    deployment fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@
 module.exports = {
     apps: [{
       name: "BrainSpark",
-      script: "/home/p/production/BrainSpark/venv/bin/gunicorn",
+      script: "/home/pi/production/BrainSpark/venv/bin/gunicorn",
       args: "--config gunicorn_config.py core.wsgi:application",
-      cwd: "/home/p/production/BrainSpark",
-      interpreter: "/home/p/production/BrainSpark/venv/bin/python",
+      cwd: "/home/pi/production/BrainSpark",
+      interpreter: "/home/pi/production/BrainSpark/venv/bin/python",
     }]
   }
